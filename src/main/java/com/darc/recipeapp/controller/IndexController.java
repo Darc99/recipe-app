07a7f888eb84205a899609bcbf093997cb439954
Darc99 <lennyyhugoh@gmail.com>
2020-06-24 17:26:02 +0100

    created recipe service and parsed it to the controller and page

--- src/main/java/com/darc/recipeapp/controller/IndexController.java
+++ src/main/java/com/darc/recipeapp/controller/IndexController.java
@@ -4,7 +4,10 @@ import com.darc.recipeapp.domain.Category;
 import com.darc.recipeapp.domain.UnitOfMeasure;
 import com.darc.recipeapp.repositories.CategoryRepository;
 import com.darc.recipeapp.repositories.UnitOfMeasureRepository;
+import com.darc.recipeapp.service.RecipeService;
+import com.darc.recipeapp.service.RecipeServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,22 +15,15 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String showIndex() {
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
-
-        System.out.println("The Cat id is " + categoryOptional.get().getId());
-        System.out.println("The Uom id is " + unitOfMeasureOptional.get().getId());
+    public String showIndex(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
