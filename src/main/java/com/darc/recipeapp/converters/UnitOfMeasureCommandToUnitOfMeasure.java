91026bcb32725771153d75d14105047e6553ce8c
Darc99 <lennyyhugoh@gmail.com>
2020-08-01 23:18:49 +0100

    added to uomc and uom converter

--- src/main/java/com/darc/recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/com/darc/recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -1,4 +1,27 @@
 package com.darc.recipeapp.converters;
 
-public class UnitOfMeasureCommandToUnitOfMeasure {
+import com.darc.recipeapp.commands.UnitOfMeasureCommand;
+import com.darc.recipeapp.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure>  {
+
+    @Nullable
+    @Synchronized
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand source) {
+
+        if (source == null) {
+            return null;
+        }
+
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+        return uom;
+    }
 }
--- src/main/java/com/darc/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
+++ src/main/java/com/darc/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -1,4 +1,26 @@
 package com.darc.recipeapp.converters;
 
-public class UnitOfMeasureToUnitOfMeasureCommand {
+import com.darc.recipeapp.commands.UnitOfMeasureCommand;
+import com.darc.recipeapp.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+
+        if (unitOfMeasure != null) {
+            final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+            uomc.setId(unitOfMeasure.getId());
+            uomc.setDescription(unitOfMeasure.getDescription());
+            return uomc;
+        }
+        return null;
+    }
 }
