4d284870860457b808bc912a01e44078423b1856
Darc99 <lennyyhugoh@gmail.com>
2020-08-03 01:41:55 +0100

    tests for both notes converters

---
+++ src/test/java/com/darc/recipeapp/converters/NotesCommandToNotesTest.java
@@ -0,0 +1,46 @@
+package com.darc.recipeapp.converters;
+
+import com.darc.recipeapp.commands.NotesCommand;
+import com.darc.recipeapp.domain.Notes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotesCommandToNotesTest {
+
+    public static final Long ID_VALUE = new Long(1L);
+    public static final String RECIPE_NOTES = "Notes";
+    NotesCommandToNotes converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new NotesCommandToNotes();
+    }
+
+    @Test
+    void testNull() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    void testEmptyObject(){
+        assertNotNull(converter.convert(new NotesCommand()));
+    }
+
+    @Test
+    void convert() {
+        //given
+        NotesCommand notesCommand = new NotesCommand();
+        notesCommand.setId(ID_VALUE);
+        notesCommand.setRecipeNotes(RECIPE_NOTES);
+
+        //when
+        Notes notes = converter.convert(notesCommand);
+
+        //then
+        assertNotNull(notes);
+        assertEquals(ID_VALUE, notes.getId());
+        assertEquals(RECIPE_NOTES, notes.getRecipeNotes());
+    }
+}
---
+++ src/test/java/com/darc/recipeapp/converters/NotesToNotesCommandTest.java
@@ -0,0 +1,45 @@
+package com.darc.recipeapp.converters;
+
+import com.darc.recipeapp.commands.NotesCommand;
+import com.darc.recipeapp.domain.Notes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotesToNotesCommandTest {
+
+    public static final Long ID_VALUE = new Long(1L);
+    public static final String RECIPE_NOTES = "Notes";
+    NotesToNotesCommand converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new NotesToNotesCommand();
+    }
+
+    @Test
+    void testNull() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    void testEmptyObject(){
+        assertNotNull(converter.convert(new Notes()));
+    }
+
+    @Test
+    void convert() {
+        //given
+        Notes notes = new Notes();
+        notes.setId(ID_VALUE);
+        notes.setRecipeNotes(RECIPE_NOTES);
+
+        //when
+        NotesCommand notesCommand = converter.convert(notes);
+
+        //then
+        assertEquals(ID_VALUE, notesCommand.getId());
+        assertEquals(RECIPE_NOTES, notesCommand.getRecipeNotes());
+    }
+}
