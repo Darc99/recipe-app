980844b4d9b4ff5c7c8ad8d35bd931b0a937d149
Darc99 <lennyyhugoh@gmail.com>
2020-09-09 18:57:17 +0100

    added number format exception

--- src/test/java/com/darc/recipeapp/controller/RecipeControllerTest.java
+++ src/test/java/com/darc/recipeapp/controller/RecipeControllerTest.java
@@ -65,6 +65,14 @@ class RecipeControllerTest {
     }
 
     @Test
+    void testGetRecipeNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
